fb418ddb9a61792c8ac9729b1255799895e42a9e
jhonataT <jhonata.tenorio2526@gmail.com>
2022-12-17 16:07:00 -0300

    Init React project

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+export const App = () => {
+    return <main>
+        
+    </main>
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { render as Render } from "react-dom";
+import { App } from "./App";
+
+Render(<App/>, document.getElementById("root"));
